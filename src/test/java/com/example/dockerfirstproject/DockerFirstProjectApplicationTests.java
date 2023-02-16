13f30ad0a7f1a7d83a64186d1f407951bafe1c3f
RajorC <rajarshi.ece@gmail.com>
2023-02-16 12:03:09 +0530

    initial commit

---
+++ src/test/java/com/example/dockerfirstproject/DockerFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerFirstProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
